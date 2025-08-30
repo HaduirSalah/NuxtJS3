fbef73b4bb7134073ac183a05777d770149ba4a1
Hadeer Salah <Haduirsalah@gmail.com>
2025-08-31 00:40:01 +0300

    6- Assets

--- app.vue
+++ app.vue
@@ -7,9 +7,15 @@
 </template>
 
 <style>
+@font-face {
+  font-family: Cairo;
+  src: url(/fonts/Cairo-Black.ttf);
+}
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: Cairo, sans-serif;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <h1>Welcome to the Home Page</h1>
+    <h1>الصفحة الرئيسية</h1>
+    <!-- <h1>Welcome to the Home Page</h1> -->
     <!-- <img src="@/assets/Cover.jpg" width="300" alt="Cover Image" />
     <img src="../assets/Cover.jpg" width="300" alt="Cover Image" /> -->
     <!-- <img src="~/assets/Cover.jpg" width="300" alt="Cover Image" /> -->
@@ -8,3 +9,9 @@
     <p>This is the home page of our Nuxt.js application.</p>
   </div>
 </template>
+<script>
+// import '@/assets/style.css';
+export default {
+  name: "HomePage",
+};
+</script>
