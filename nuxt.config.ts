2015172a411137663e9725d12eda4db552e8d671
Hadeer Salah <Haduirsalah@gmail.com>
2025-08-31 00:57:27 +0300

    7- SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,24 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["@/assets/style.css"],
+  // way 3: using nuxt.config.ts for global meta tags 
+  app: {
+    head: {
+      title: "My Nuxt.js Application",
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          name: "description",
+          content: "My Nuxt.js Application",
+        },
+        {
+          name: "keywords",
+          content: "development, Vuejs, Nuxtjs",
+        },
+      ],
+    },
+  },
 });
--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,30 @@
 <template>
     <div class="about-page">
+        <!-- way 1: using Head tag for local meta tags -->
+        <!-- <Head>
+            <title>About Us - My Nuxt App</title>
+            <meta name="description" content="Learn more about us on this page." />
+            <meta name="keywords" content="development, Vuejs, Nuxtjs"></meta>
+        </Head>
         <h1>About Us</h1>
-        <p>This is the about page of our Nuxt.js application.</p>
+        <p>This is the about page of our Nuxt.js application.</p> -->
     </div>
 </template>
+
+<script setup>
+// composition API
+// way 2.1: using useHead for local meta tags
+// useHead({
+//     title: 'About Us - My Nuxt App',
+//     meta: [
+//         { name: 'description', content: 'Learn more about us on this page.' },
+//         { name: 'keywords', content: 'development, Vuejs, Nuxtjs' }
+//     ]
+// });
+// way 2.2: using useSeoMeta for local meta tags
+// useSeoMeta({
+//     title: 'About Us - My Nuxt App',
+//     description: 'Learn more about us on this page.',
+//     keywords: 'development, Vuejs, Nuxtjs'
+// });
+</script>
